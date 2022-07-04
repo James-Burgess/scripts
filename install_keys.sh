9708b9cc13188a8fb6dde3182da7a41447b13524
jimmy <jimmy@pop-os.localdomain>
2022-07-04 11:53:00 +0200

    some scripts from getting i3 running on popos

---
+++ install_keys.sh
@@ -0,0 +1,19 @@
+echo "installing deps"
+
+sudo apt-get update -Yqq
+sudo apt-get install git gnupg rng-tools -y
+
+
+echo "installing yubikey manager"
+sudo apt install yubikey-manager -y
+
+echo "grabbing keys"
+
+
+echo "installing gopass"
+
+#wget 
+
+#sudo apt install golang-go gopass -y
+#go install github.com/gopasspw/gopass@latest
+
